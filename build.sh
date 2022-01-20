57137ecbcd9fdbc31e67d31c5503faf8569ab23c
root <root@f36.vpnservice.company>
2022-01-20 18:58:52 +0000

    m

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ DISTRO=${1:-fedora}
 
 ccmd="docker-compose -f container-compose.yaml build"
 eval "$ccmd"
-`exit
+exit
 
 cmd="docker build -f $DISTRO.Dockerfile -t $DISTRO-pueue-container --target $DISTRO-pueue-container ."
 eval "$cmd"
