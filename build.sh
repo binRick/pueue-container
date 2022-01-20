8515c6de07afe8ea121ebde946ab71883e7aa5d9
root <root@f36.vpnservice.company>
2022-01-20 21:51:54 +0000

    m

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ CM=${CM:-docker}
 #command yaml2json >/dev/null || pip3 install json2yaml
 
 tf=./.$DISTRO-docker-compose.yaml
-./render_container_compose.sh | tee $tf
+./render_container_compose.sh 2>&1 | tee $tf
 cmd="$CM-compose -f $tf build"
 ansi --yellow --italic "$cmd"
 #exit
--- render_container_compose.sh
+++ render_container_compose.sh
@@ -2,6 +2,6 @@
 set -eou pipefail
 DISTRO=${DISTRO:-fedora}
 cmd="command cat container-compose.yaml|sed 's/alpine.Dockerfile/$DISTRO.Dockerfile/g'|sed 's/alpine-/$DISTRO-/g'|yaml2json 2>/dev/null|json2yaml"
-eval "$cmd"
-cmd="command cat container-compose.yaml|sed 's/fedora.Dockerfile/$DISTRO.Dockerfile/g'|sed 's/fedora-/$DISTRO-/g'|yaml2json 2>/dev/null|json2yaml"
+#eval "$cmd"
+#cmd="command cat container-compose.yaml|sed 's/fedora.Dockerfile/$DISTRO.Dockerfile/g'|sed 's/fedora-/$DISTRO-/g'|yaml2json 2>/dev/null|json2yaml"
 eval "$cmd"
