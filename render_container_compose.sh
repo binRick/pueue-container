3c2cc8f0f7faf01ddcc66138c4f3350b9c945af2
root <root@f36.vpnservice.company>
2022-01-20 21:45:07 +0000

    m

--- render_container_compose.sh
+++ render_container_compose.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eou pipefail
 DISTRO=${DISTRO:-fedora}
-cmd="command cat container-compose.yaml|sed 's/DF alpine.Dockerfile/DF $DISTRO.Dockerfile/g'|sed 's/IMG alpine-/IMG $DISTRO-/g'|yaml2json 2>/dev/null|json2yaml"
+cmd="command cat container-compose.yaml|sed 's/alpine.Dockerfile/$DISTRO.Dockerfile/g'|sed 's/alpine-/$DISTRO-/g'|yaml2json 2>/dev/null|json2yaml"
 eval "$cmd"
-cmd="command cat container-compose.yaml|sed 's/DF fedora.Dockerfile/DF $DISTRO.Dockerfile/g'|sed 's/IMG fedora-/IMG $DISTRO-/g'|yaml2json 2>/dev/null|json2yaml"
+cmd="command cat container-compose.yaml|sed 's/fedora.Dockerfile/$DISTRO.Dockerfile/g'|sed 's/fedora-/$DISTRO-/g'|yaml2json 2>/dev/null|json2yaml"
 eval "$cmd"
