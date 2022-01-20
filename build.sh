33004c058adc22f58687ae18b912ec18efd70ca3
Richard Blundell <richard@mac.pro>
2022-01-20 20:19:17 +0000

    m

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -eou pipefail
 DISTRO=${DISTRO:-fedora}
-command yaml2json >/dev/null || pip3 install json2yaml
+#command yaml2json >/dev/null || pip3 install json2yaml
 
 tf=./.$DISTRO-docker-compose.yaml
-./render_container_compose.sh > $tf
-cmd="docker-compose -f $tf build"
+#timeout 1 ./render_container_compose.sh > $tf
+cmd="podman-compose -f $tf build"
 ansi --yellow --italic "$cmd"
 #exit
 eval "$cmd"
