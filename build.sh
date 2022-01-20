06c362ec36e3781681c4c63297e36a9f4f80e76a
root <root@f36.vpnservice.company>
2022-01-20 18:58:34 +0000

    m

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
 set -eou pipefail
 DISTRO=${1:-fedora}
 
+ccmd="docker-compose -f container-compose.yaml build"
+eval "$ccmd"
+`exit
+
 cmd="docker build -f $DISTRO.Dockerfile -t $DISTRO-pueue-container --target $DISTRO-pueue-container ."
 eval "$cmd"
