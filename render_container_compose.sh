962295db2817b69c221d589f7d351e8a4250e045
Richard Blundell <richard@mac.pro>
2022-01-20 21:19:19 +0000

    m

--- render_container_compose.sh
+++ render_container_compose.sh
@@ -2,4 +2,5 @@
 set -eou pipefail
 DISTRO=${DISTRO:-fedora}
 cmd="command cat container-compose.yaml|sed 's/DF alpine.Dockerfile/DF $DISTRO.Dockerfile/g'|sed 's/IMG alpine-/IMG $DISTRO-/g'|yaml2json 2>/dev/null|json2yaml"
+cmd="command cat container-compose.yaml|sed 's/DF fedora.Dockerfile/DF $DISTRO.Dockerfile/g'|sed 's/IMG fedora-/IMG $DISTRO-/g'|yaml2json 2>/dev/null|json2yaml"
 eval "$cmd"
