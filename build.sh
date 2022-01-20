0ffdc9b42d9e00641e477a650a9f12e7d0bbad03
root <root@f36.vpnservice.company>
2022-01-20 20:22:18 +0000

    m

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ DISTRO=${DISTRO:-fedora}
 #command yaml2json >/dev/null || pip3 install json2yaml
 
 tf=./.$DISTRO-docker-compose.yaml
-#timeout 1 ./render_container_compose.sh > $tf
+./render_container_compose.sh | tee $tf
 cmd="podman-compose -f $tf build"
 ansi --yellow --italic "$cmd"
 #exit
 eval "$cmd"
-exit
+#exit
