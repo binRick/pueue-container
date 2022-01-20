187be3fffb01fecc0c4803325e544064dcf99b15
root <root@f36.vpnservice.company>
2022-01-20 20:04:17 +0000

    m

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eou pipefail
 DISTRO=${DISTRO:-fedora}
+command yaml2json >/dev/null || pip3 install json2yaml
 
 tf=./.$DISTRO-docker-compose.yaml
 ./render_container_compose.sh > $tf
