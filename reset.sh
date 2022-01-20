b429d39893748708e8ab810bcbc4f516fa540b26
root <root@f36.vpnservice.company>
2022-01-20 21:39:56 +0000

    m

---
+++ reset.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set +e
+docker ps -aq|xargs -I % docker rm -f %
+podman ps -aq|xargs -I % podman rm -f %
+docker images -aq|xargs -I % docker rmi -f %
+podman images -aq|xargs -I % podman rmi -f %
