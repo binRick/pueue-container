e7c31d1e3e811b658b35158b793a2dadc4e267b0
Richard Blundell <richard@mac.pro>
2022-01-21 04:05:19 +0000

    m

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -eou pipefail
-DISTRO=${DISTRO:-fedora};shift||true
-N=$DISTRO-c0
-cmd="${@:-ls /etc}"
+SVC=$1;shift
+ARGS="${@:-sh}"
 
-cmd="docker run --privileged --rm -it --name $N  --volume /sys/fs/cgroup:/sys/fs/cgroup:ro $DISTRO-pueue $cmd"
-cmd="docker-compose -f container-compose.yaml up"
+#cmd="docker run --privileged --rm -it --name $N  --volume /sys/fs/cgroup:/sys/fs/cgroup:ro $DISTRO-pueue $cmd"
+#cmd="docker-compose -f container-compose.yaml up"
+tf=.$DISTRO-container-compose.yaml
+./render_container_compose.sh > $tf
+cmd="$CM-compose -f $tf run --name c11 -w /root  --rm -- iodined0 $ARGS"
 >&2 ansi --yellow --italic "$cmd"
 eval "$cmd"
