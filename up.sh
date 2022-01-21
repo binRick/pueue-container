a415e05555ea73e6d1e46635ddcc817d532b3079
root <root@f36.vpnservice.company>
2022-01-21 02:17:58 +0000

    m

--- up.sh
+++ up.sh
@@ -5,6 +5,6 @@ C=${1:-}
 
 tf=./.$DISTRO-docker-compose.yaml
 ./render_container_compose.sh 2>&1 | tee $tf
-cmd="$CM-compose -f $tf up --force-recreate --remove-orphans $C --build"
+cmd="$CM-compose -f $tf up --force-recreate --remove-orphans $C"
 >&2 ansi --yellow --italic "$cmd"
 eval "$cmd"
