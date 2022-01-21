f3c922ff64756b1733ad0d99804de2b6e18404cb
Richard Blundell <richard@mac.pro>
2022-01-21 03:30:45 +0000

    m

--- render_container_compose.sh
+++ render_container_compose.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -eou pipefail
 DISTRO=${DISTRO:-fedora}
-cmd="command cat container-compose.yaml|sed 's/alpine.Dockerfile/$DISTRO.Dockerfile/g'|sed 's/alpine-/$DISTRO-/g'|yaml2json 2>/dev/null|json2yaml"
+if [[ "$DISTRO" == fedora ]]; then
+  cmd="command cat container-compose.yaml|sed 's/alpine/$DISTRO/g'|yaml2json 2>/dev/null|json2yaml"
+else
+  cmd="command cat container-compose.yaml|sed 's/fedora/$DISTRO/g'|yaml2json 2>/dev/null|json2yaml"
+fi
 #eval "$cmd"
 #cmd="command cat container-compose.yaml|sed 's/fedora.Dockerfile/$DISTRO.Dockerfile/g'|sed 's/fedora-/$DISTRO-/g'|yaml2json 2>/dev/null|json2yaml"
 eval "$cmd"
