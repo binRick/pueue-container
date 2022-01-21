c2644566edbe2a3004eff5ee1563ff6b5b4eb306
root <root@f36.vpnservice.company>
2022-01-21 01:22:36 +0000

    m

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ CM=${CM:-podman}
 command -v yaml2json >/dev/null || pip3 install json2yaml
 
 tf=./.$DISTRO-docker-compose.yaml
+
 ./render_container_compose.sh 2>&1 | tee $tf
+
 cmd="$CM-compose -f $tf build"
 ansi --yellow --italic "$cmd"
-#exit
 eval "$cmd"
-#exit
