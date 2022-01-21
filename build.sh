9e68750d5ffcfcae15016372e14d8047f1d6704d
Richard Blundell <richard@mac.pro>
2022-01-21 06:41:10 +0000

    m

--- build.sh
+++ build.sh
@@ -10,6 +10,9 @@ tf=.$DISTRO-container-compose.yaml
 
 ./render_container_compose.sh 2>&1 | tee $tf
 
+bc="$CM-compose --ansi always  -f $tf build --no-cache --progress=tty --pull --force-rm"
+ansi --magenta --underline --italic "$bc"
+
 cmd="$CM-compose -f $tf pull && $CM-compose -f $tf build"
 ansi --yellow --italic "$cmd"
 
--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ ARGS="${@:-sh}"
 #cmd="docker run --privileged --rm -it --name $N  --volume /sys/fs/cgroup:/sys/fs/cgroup:ro $DISTRO-pueue $cmd"
 tf=.$DISTRO-container-compose.yaml
 ./render_container_compose.sh > $tf
-cmd="$CM-compose -f $tf run --name $SVC-run -w /root  --rm -- $SVC $ARGS"
+cmd="$CM-compose -f $tf run --name $SVC-run -p 18105:18105 -w /root  --rm -- $SVC $ARGS"
 >&2 ansi --yellow --italic "$cmd"
 eval "$cmd"
