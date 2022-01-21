6da7b87fddb573bce96da987e10aaa8cc113b99a
root <root@f36.vpnservice.company>
2022-01-21 01:16:20 +0000

    m

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ set -eou pipefail
 source .envrc
 git pull||git pull
 DISTRO=${DISTRO:-fedora}
-CM=${CM:-docker}
-#command yaml2json >/dev/null || pip3 install json2yaml
+CM=${CM:-podman}
+command yaml2json >/dev/null || pip3 install json2yaml
 
 tf=./.$DISTRO-docker-compose.yaml
 ./render_container_compose.sh 2>&1 | tee $tf
