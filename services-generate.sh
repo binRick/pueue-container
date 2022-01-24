29d0079c795d289679ade1712fd2557394a1e065
Richard Blundell <richard@mac.pro>
2022-01-24 01:43:04 +0000

    m

--- services-generate.sh
+++ services-generate.sh
@@ -10,7 +10,8 @@ doit() {
 	(
 		\cat $CF | grep '^services:$' -B 9999
 		while read -r svc; do
-			echo -e "    $svc:\n$(./service-get-yaml.sh $svc)\n"
+      y="$(./service-get-yaml.sh $svc)"
+			echo -e "\n $svc:\n$y\n"
 		done < <(./services-ls-active.sh)
 		#| yaml2json | json2yaml
 	) | stdbuf -oL cat
