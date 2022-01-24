f4e94b3574fc77f469748a9e9db5a071f454c517
root <root@f36.vpnservice.company>
2022-01-24 23:29:22 +0000

    m

--- push.sh
+++ push.sh
@@ -1,1 +1,4 @@
-docker push docker.io/vpntechdockerhub/pueue:alpine
+#!/bin/bash
+set -eou pipefail; set -x
+echo -e "$DHP" | docker login docker.io -u vpntechdockerhub --password-stdin
+docker push docker.io/vpntechdockerhub/pueue:$DISTRO
--- tag.sh
+++ tag.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -eou pipefail
 set -x
-#docker rmi -f pueue
-#podman build -f fedora.Dockerfile -t pueue --target pueue .
 
-#docker tag localhost/pueue:latest docker.io/vpntechdockerhub/pueue:latest
-#docker tag pueue docker.io/vpntechdockerhub/pueue:latest
-
-#docker push docker.io/vpntechdockerhub/pueue:latest
-docker tag alpine-pueue:latest docker.io/vpntechdockerhub/pueue:alpine
+docker tag $DISTRO-pueue:latest docker.io/vpntechdockerhub/pueue:$DISTRO
