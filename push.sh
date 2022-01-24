deac9d6ac666c3948c8e42543b4e50c7d145c54f
Richard Blundell <richard@mac.pro>
2022-01-24 23:30:57 +0000

    m

--- push.sh
+++ push.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -eou pipefail; set -x
+source .envrc.sh
+
 echo -e "$DHP" | docker login docker.io -u vpntechdockerhub --password-stdin
 docker push docker.io/vpntechdockerhub/pueue:$DISTRO
