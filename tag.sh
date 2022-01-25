bd4b03dd9aad7dcd6beaaec6af17dced228e18a7
Richard Blundell <richard@mac.pro>
2022-01-25 19:25:12 +0000

    m

--- tag.sh
+++ tag.sh
@@ -5,3 +5,4 @@ set -x
 docker tag $DISTRO-pueue:latest docker.io/vpntechdockerhub/pueue:$DISTRO
 docker tag $DISTRO-restic-rest-server:latest docker.io/vpntechdockerhub/pueue:$DISTRO-restic
 docker tag $DISTRO-ttyd:latest docker.io/vpntechdockerhub/pueue:$DISTRO-ttyd
+docker tag $DISTRO-gottyd:latest docker.io/vpntechdockerhub/pueue:$DISTRO-gottyd
